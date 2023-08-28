c0083e5c2b2bba7952064a715a4e87662d915d18
masterbhuvan1 <bhuvanjain212003@gmail.com>
2023-08-29 00:57:53 +0530

    fifth commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,6 @@ import { fetchDataFromApi } from "./utils/api";
 import { Details } from "./pages/details/Details";
 import SearchResult from "./pages/searchResult/SearchResult";
 
-import { PageNotFound } from "./pages/404/pageNotFound";
-
 import Explore from "./pages/explore/explore";
 import Footer from "./components/footer/Footer";
 import { getGenres } from "./store/homeSlice";
@@ -72,7 +70,7 @@ function App() {
         <Route path="/Movix/search/:query" element={<SearchResult />} />
         <Route path="/explore/:mediaType" element={<Explore />} />
         <Route path="/Movix/explore/:mediaType" element={<Explore />} />
-        <Route path="/" element={<PageNotFound />} />
+        {/* <Route path="/" element={<PageNotFound />} /> */}
       </Routes>
       <Footer />
     </BrowserRouter>
